e8969a40cbbb06e24ed124be0dbf0de1fafcc156
Trevor <travo301985@gmail.com>
2024-03-22 08:46:40 +0200

    project setup

---
+++ src/pages/Todo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Todo() {
+  return (
+    <div>
+      <a href="/" rel="noopener noreferrer">
+        Dashboard
+      </a>
+    </div>
+  );
+}
+
+export default Todo;
